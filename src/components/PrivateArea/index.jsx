076ac48b67009438b01d5e42e3334cb41c3d6b46
Sallys Carlos <sallys.carlos93@gmail.com>
2022-11-13 14:02:46 -0300

    feat: add sidenav

--- src/components/PrivateArea/index.jsx
+++ src/components/PrivateArea/index.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import S from "./style";
 import Header from "../Header";
+import Sidebar from "./SideBar";
 
 const PrivateArea = () => {
   return (
     <S.Container>
       <Header />
       <main>
-        {/* <Sidebar /> */}
+        <Sidebar />
         <Outlet />
       </main>
     </S.Container>
