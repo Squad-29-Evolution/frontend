2753d4cbff255f86928cb44025844d0f1067cae6
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-11-14 19:17:12 -0300

    feat: finish function for create content

--- src/pages/AdminContents/components/Modal/index.jsx
+++ src/pages/AdminContents/components/Modal/index.jsx
@@ -6,6 +6,7 @@ import schema from "./validation";
 import api from "../../../../api";
 import { useState, useEffect } from "react";
 import SelectBox from "../../../../components/SelectBox";
+import LoadingSmall from "../../../../components/LoadingSmall";
 
 // eslint-disable-next-line no-unused-vars
 const Modal = ({ closeModal, setData, notify, token }) => {
@@ -15,6 +16,7 @@ const Modal = ({ closeModal, setData, notify, token }) => {
   const [trailLabel, setTrailLabel] = useState("Selecione uma trilha");
   const [categorysData, setCategoryData] = useState([]);
   const [trailsData, setTrailData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -23,6 +25,7 @@ const Modal = ({ closeModal, setData, notify, token }) => {
   } = useForm({ resolver: yupResolver(schema) });
 
   const ToggleActiveCategory = () => {
+    console.log("abriu");
     setCategoryIsOpen((prev) => !prev);
   };
 
@@ -41,6 +44,8 @@ const Modal = ({ closeModal, setData, notify, token }) => {
   };
 
   const getAllFetch = async () => {
+    setIsLoading(true);
+
     const config = {
       headers: { Authorization: `Bearer ${token}` },
     };
@@ -51,6 +56,7 @@ const Modal = ({ closeModal, setData, notify, token }) => {
     ]);
     setCategoryData(categorys);
     setTrailData(trails);
+    return setIsLoading(false);
   };
 
   useEffect(() => {
@@ -61,21 +67,25 @@ const Modal = ({ closeModal, setData, notify, token }) => {
     //setIsLoading(true);
     console.log(errors);
 
-    console.log(formData);
-    /* try {
+    const trail_id = parseInt(formData.trail);
+    const category_id = parseInt(formData.category);
+
+    const newData = { ...formData, trail_id, category_id, type: "link" };
+    console.log("nova :", newData);
+    try {
       const config = {
         headers: { Authorization: `Bearer ${token}` },
       };
-      const { data } = await api.post("/trail/create", formData, config);
-
-      const { name, description, hours, icon, id } = data;
-      setData((prev) => [...prev, { name, description, hours, icon, id }]);
-      notify("success", "Trilha adicionada com sucesso");
-      return closeModal();
+      const { data } = await api.post("/contents/create", newData, config);
+      console.log(data);
+      //const { name, description, hours, icon, id } = data;
+      //setData((prev) => [...prev, { name, description, hours, icon, id }]);
+      notify("success", "Conteudo adicionada com sucesso");
+      //return closeModal();
     } catch (error) {
       console.log(error);
-      notify("error", "Ocorreu um error ao criar trilha");
-    } */
+      notify("error", "Ocorreu um error ao criar conteudo");
+    }
   };
 
   return (
@@ -83,74 +93,80 @@ const Modal = ({ closeModal, setData, notify, token }) => {
       <S.Container>
         <S.Title>Adicionar novo conteúdo</S.Title>
         <S.Form onSubmit={handleSubmit(onSubmit)}>
-          <S.WrapperGeneric>
-            <InputLabel
-              type="text"
-              register={register}
-              name={"title"}
-              label={"Nome"}
-            />
-            <S.SpanError>{errors.title?.message}</S.SpanError>
-          </S.WrapperGeneric>
+          {isLoading ? (
+            <LoadingSmall />
+          ) : (
+            <>
+              <S.WrapperGeneric>
+                <InputLabel
+                  type="text"
+                  register={register}
+                  name={"title"}
+                  label={"Nome"}
+                />
+                <S.SpanError>{errors.title?.message}</S.SpanError>
+              </S.WrapperGeneric>
 
-          <S.WrapperGeneric>
-            <S.Label>Descrição</S.Label>
-            <S.Description
-              placeholder="Descrição do conteúdo"
-              name="description"
-              {...register("description")}
-            />
-            <S.SpanError>{errors.description?.message}</S.SpanError>
-          </S.WrapperGeneric>
+              <S.WrapperGeneric>
+                <S.Label>Descrição</S.Label>
+                <S.Description
+                  placeholder="Descrição do conteúdo"
+                  name="description"
+                  {...register("description")}
+                />
+                <S.SpanError>{errors.description?.message}</S.SpanError>
+              </S.WrapperGeneric>
 
-          <S.WrapperGeneric>
-            <InputLabel
-              type="text"
-              register={register}
-              name={"link"}
-              label={"Link do conteúdo (URL)"}
-            />
-            <S.SpanError>{errors.link?.message}</S.SpanError>
-          </S.WrapperGeneric>
+              <S.WrapperGeneric>
+                <InputLabel
+                  type="text"
+                  register={register}
+                  name={"link"}
+                  label={"Link do conteúdo (URL)"}
+                />
+                <S.SpanError>{errors.link?.message}</S.SpanError>
+              </S.WrapperGeneric>
 
-          <S.WrapperGeneric>
-            <S.Label>Categorias:</S.Label>
+              <S.WrapperGeneric>
+                <S.Label>Categorias:</S.Label>
 
-            <SelectBox
-              Label={categoryLabel}
-              setLabel={setCategoryLabel}
-              isActive={categoryIsOpen}
-              register={register}
-              name="category"
-              toggleActive={ToggleActiveCategory}
-              data={categorysData}
-            />
+                <SelectBox
+                  Label={categoryLabel}
+                  setLabel={setCategoryLabel}
+                  isActive={categoryIsOpen}
+                  register={register}
+                  name="category"
+                  toggleActive={ToggleActiveCategory}
+                  data={categorysData}
+                />
 
-            <S.SpanError>{errors.category?.message}</S.SpanError>
-          </S.WrapperGeneric>
+                <S.SpanError>{errors.category?.message}</S.SpanError>
+              </S.WrapperGeneric>
 
-          <S.WrapperGeneric>
-            <S.Label>Trilhas:</S.Label>
+              <S.WrapperGeneric>
+                <S.Label>Trilhas:</S.Label>
 
-            <SelectBox
-              Label={trailLabel}
-              setLabel={setTrailLabel}
-              isActive={trailIsOpen}
-              register={register}
-              name="trail"
-              toggleActive={ToggleActiveTrail}
-              data={trailsData}
-            />
+                <SelectBox
+                  Label={trailLabel}
+                  setLabel={setTrailLabel}
+                  isActive={trailIsOpen}
+                  register={register}
+                  name="trail"
+                  toggleActive={ToggleActiveTrail}
+                  data={trailsData}
+                />
 
-            <S.SpanError>{errors.category?.message}</S.SpanError>
-          </S.WrapperGeneric>
+                <S.SpanError>{errors.category?.message}</S.SpanError>
+              </S.WrapperGeneric>
 
-          <S.WrapperButton>
-            <S.Button type="reset" onClick={closeModal}>
-              Cancelar
-            </S.Button>
-            <S.Button type="submit">Criar</S.Button>
-          </S.WrapperButton>
+              <S.WrapperButton>
+                <S.Button type="reset" onClick={closeModal}>
+                  Cancelar
+                </S.Button>
+                <S.Button type="submit">Criar</S.Button>
+              </S.WrapperButton>
+            </>
+          )}
         </S.Form>
       </S.Container>
       <S.BG />
