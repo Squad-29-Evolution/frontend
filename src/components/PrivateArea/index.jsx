a2209bca18e734848e5a2badc855048ea76651cd
Sallys Carlos <sallys.carlos93@gmail.com>
2022-11-14 09:57:07 -0300

    feat: get current route and visibility sidebar

--- src/components/PrivateArea/index.jsx
+++ src/components/PrivateArea/index.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import S from "./style";
 import Header from "../Header";
 import Sidebar from "./SideBar";
 
 const PrivateArea = () => {
+  const location = useLocation();
+
   return (
     <S.Container>
       <Header />
       <S.Main>
-        <Sidebar />
+        {location.pathname == "/content" ? null : <Sidebar />}
         <S.ContentOutlet>
           <Outlet />
         </S.ContentOutlet>
